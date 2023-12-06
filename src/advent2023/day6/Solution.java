56264b6ac69c89202a2daab18f9678c18d3d4848
Tomas Hamsa <okularek@centrum.cz>
2023-12-06 12:42:39 +0100

    Solve day 6

--- src/advent2023/day6/Solution.java
+++ src/advent2023/day6/Solution.java
@@ -2,6 +2,12 @@ package src.advent2023.day6;
 
 import src.PuzzleSolver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Solution extends PuzzleSolver {
@@ -12,26 +18,53 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public String getExampleInput1() {
-        return "";
+        return "Time:      7  15   30\n" +
+                "Distance:  9  40  200";
     }
 
     @Override
     public String getExampleOutput1() {
-        return "";
+        return "288";
     }
 
     @Override
     public String getExampleOutput2() {
-        return null;
+        return "71503";
     }
 
     @Override
     public String solvePartOne(Stream<String> lines) {
-        return "";
+        String[] linesArray = lines.toArray(String[]::new);
+
+        StringTokenizer tokens = new StringTokenizer(linesArray[0], " ");
+        tokens.nextToken();
+        List<Integer> times = new ArrayList<>(4);
+        while (tokens.hasMoreTokens()) {
+            times.add(Integer.parseInt(tokens.nextToken()));
+        }
+
+        int[] waysOfBeatingRecord = new int[times.size()];
+        tokens = new StringTokenizer(linesArray[1], " ");
+        tokens.nextToken();
+        for (int i = 0; i < times.size(); i++) {
+            int time = times.get(i);
+            int record = Integer.parseInt(tokens.nextToken());
+            waysOfBeatingRecord[i] = (int) IntStream.range(1, time).parallel()
+                    .filter(millisecondsHoldingButton -> millisecondsHoldingButton * (time - millisecondsHoldingButton) > record)
+                    .count();
+        }
+
+        return Arrays.stream(waysOfBeatingRecord).reduce(1, (a, b) -> a * b) + "";
     }
 
     @Override
     public String solvePartTwo(Stream<String> lines) {
-        return "";
+        String[] linesArray = lines.toArray(String[]::new);
+        int time = Integer.parseInt(linesArray[0].replaceFirst("Time:", "").replaceAll("\\s", ""));
+        long distance = Long.parseLong(linesArray[1].replaceFirst("Distance:", "").replaceAll("\\s", ""));
+
+        return LongStream.range(1, time).parallel()
+                .filter(millisecondsHoldingButton -> millisecondsHoldingButton * (time - millisecondsHoldingButton) > distance)
+                .count() + "";
     }
 }
---
+++ src/advent2023/day7/Solution.java
@@ -0,0 +1,37 @@
+package src.advent2023.day7;
+
+import src.PuzzleSolver;
+
+import java.util.stream.Stream;
+
+public class Solution extends PuzzleSolver {
+
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    @Override
+    public String getExampleInput1() {
+        return "";
+    }
+
+    @Override
+    public String getExampleOutput1() {
+        return "";
+    }
+
+    @Override
+    public String getExampleOutput2() {
+        return null;
+    }
+
+    @Override
+    public String solvePartOne(Stream<String> lines) {
+        return "";
+    }
+
+    @Override
+    public String solvePartTwo(Stream<String> lines) {
+        return "";
+    }
+}
