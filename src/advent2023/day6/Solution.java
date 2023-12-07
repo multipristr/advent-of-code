4b5d026bbc1c306115a30d6764d9d08f2e3295cf
Tomas Hamsa <okularek@centrum.cz>
2023-12-07 21:19:13 +0100

    Add commented quadratic roots to day 6

--- src/advent2023/day6/Solution.java
+++ src/advent2023/day6/Solution.java
@@ -64,6 +64,13 @@ public class Solution extends PuzzleSolver {
         int time = Integer.parseInt(linesArray[0].replaceFirst("Time:", "").replaceAll("\\s", ""));
         long distance = Long.parseLong(linesArray[1].replaceFirst("Distance:", "").replaceAll("\\s", ""));
 
+        /*
+        var sqrt = Math.sqrt(Math.pow(time, 2) - 4 * distance);
+        var start = Math.floor((time - sqrt) / 2 + 1);
+        var end = Math.ceil((time + sqrt) / 2 - 1);
+        return (long) (end - start + 1) + "";
+         */
+
         for (long millisecondsHoldingButton = 1; millisecondsHoldingButton < time; millisecondsHoldingButton++) {
             if (millisecondsHoldingButton * (time - millisecondsHoldingButton) > distance) {
                 return 1 + time - 2 * millisecondsHoldingButton + "";
