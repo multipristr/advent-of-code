06cd6644837a7ec675e17611cde283587c41575b
Tomas Hamsa <okularek@centrum.cz>
2023-12-09 09:31:22 +0100

    Solve day 9

--- src/advent2023/day9/Solution.java
+++ src/advent2023/day9/Solution.java
@@ -2,6 +2,7 @@ package src.advent2023.day9;
 
 import src.PuzzleSolver;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Solution extends PuzzleSolver {
@@ -12,26 +13,64 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public String getExampleInput1() {
-        return "";
+        return "0 3 6 9 12 15\n" +
+                "1 3 6 10 15 21\n" +
+                "10 13 16 21 30 45";
     }
 
     @Override
     public String getExampleOutput1() {
-        return "";
+        return "114";
     }
 
     @Override
     public String getExampleOutput2() {
-        return null;
+        return "2";
     }
 
     @Override
     public String solvePartOne(Stream<String> lines) {
-        return "";
+        return lines.parallel()
+                .map(line -> line.split("\\s"))
+                .map(parts -> Arrays.stream(parts).mapToInt(Integer::parseInt).toArray())
+                .mapToInt(this::calculatePrediction)
+                .sum() + "";
+    }
+
+    private int calculatePrediction(int[] history) {
+        int[] newSequence = new int[history.length - 1];
+        boolean anyNonZero = false;
+        for (int i = 1; i < history.length; i++) {
+            int difference = history[i] - history[i - 1];
+            anyNonZero |= difference != 0;
+            newSequence[i - 1] = difference;
+        }
+        if (!anyNonZero) {
+            return history[history.length - 1];
+        }
+        return history[history.length - 1] + calculatePrediction(newSequence);
     }
 
     @Override
     public String solvePartTwo(Stream<String> lines) {
-        return "";
+        return lines.parallel()
+                .map(line -> line.split("\\s"))
+                .map(parts -> Arrays.stream(parts).mapToInt(Integer::parseInt).toArray())
+                .mapToInt(this::calculatePredictionBackwards)
+                .sum() + "";
+    }
+
+    private int calculatePredictionBackwards(int[] history) {
+        int[] newSequence = new int[history.length - 1];
+        boolean anyNonZero = false;
+        for (int i = 1; i < history.length; i++) {
+            int difference = history[i] - history[i - 1];
+            anyNonZero |= difference != 0;
+            newSequence[i - 1] = difference;
+        }
+        if (!anyNonZero) {
+            return history[0];
+        }
+        return history[0] - calculatePredictionBackwards(newSequence);
     }
 }
