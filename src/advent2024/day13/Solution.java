5250bf994f906f186b4ba23037491dfd6e54e0ad
MultiHunter <email@email.com>
2024-10-13 13:32:17 +0200

    Prepare 2024

---
+++ src/advent2024/day13/Solution.java
@@ -0,0 +1,38 @@
+package src.advent2024.day13;
+
+import src.PuzzleSolver;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Solution extends PuzzleSolver {
+
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    @Override
+    public List<String> getExampleInput1() {
+        return List.of("");
+    }
+
+    @Override
+    public List<Long> getExampleOutput1() {
+        return List.of();
+    }
+
+    @Override
+    public List<Long> getExampleOutput2() {
+        return List.of();
+    }
+
+    @Override
+    public long solvePartOne(Stream<String> lines) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public long solvePartTwo(Stream<String> lines) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
