98eedb9c6e101655ab680693585a3dd674de366d
MultiHunter <email@email.com>
2024-12-02 07:15:57 +0100

    Solve 2024 day 2 part 1

--- src/advent2024/day2/Solution.java
+++ src/advent2024/day2/Solution.java
@@ -13,12 +13,17 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public List<String> getExampleInput1() {
-        return List.of("");
+        return List.of("7 6 4 2 1\n" +
+                "1 2 7 8 9\n" +
+                "9 7 6 2 1\n" +
+                "1 3 2 4 5\n" +
+                "8 6 4 4 1\n" +
+                "1 3 6 7 9");
     }
 
     @Override
     public List<Long> getExampleOutput1() {
-        return List.of();
+        return List.of(2L);
     }
 
     @Override
@@ -28,7 +33,32 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public long solvePartOne(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return lines.map(line -> line.split("\\s+"))
+                .filter(line -> {
+                    boolean increasing = false;
+                    boolean decreasing = false;
+                    for (int i = 1; i < line.length; i++) {
+                        long first = Long.parseLong(line[i - 1]);
+                        long second = Long.parseLong(line[i]);
+                        long difference = Math.abs(first - second);
+                        if (difference > 3 || difference == 0) {
+                            return false;
+                        }
+                        if (second > first) {
+                            if (decreasing) {
+                                return false;
+                            }
+                            increasing = true;
+                        } else if (second < first) {
+                            if (increasing) {
+                                return false;
+                            }
+                            decreasing = true;
+                        }
+                    }
+                    return true;
+                })
+                .count();
     }
 
     @Override
