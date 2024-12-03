0ac3cc26a757ccce71e91caa7392a1e81fe8052b
MultiHunter <email@email.com>
2024-12-03 08:15:37 +0100

    Parse 2024 day 3

--- src/advent2024/day3/Solution.java
+++ src/advent2024/day3/Solution.java
@@ -3,6 +3,9 @@ package src.advent2024.day3;
 import src.PuzzleSolver;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution extends PuzzleSolver {
@@ -13,12 +16,12 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public List<String> getExampleInput1() {
-        return List.of("");
+        return List.of("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
     }
 
     @Override
     public List<Long> getExampleOutput1() {
-        return List.of();
+        return List.of(161L);
     }
 
     @Override
@@ -28,7 +31,15 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public long solvePartOne(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Pattern instructions = Pattern.compile(".*mul\\((?<number1>\\d{1,3}),(?<number2>\\d{1,3})\\).*");
+        String input = lines.collect(Collectors.joining());
+
+        Matcher matcher = instructions.matcher(input);
+        if (!matcher.find()) {
+            throw new IllegalStateException(instructions + " not found in " + input);
+        }
+
+        return 0L;
     }
 
     @Override
