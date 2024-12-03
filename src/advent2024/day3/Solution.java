b0b305ba9fe85aadb27d32e91fd1fdb84a445ed0
Tomas Hamsa <Tomas.Hamsa@ufst.dk>
2024-12-03 15:41:19 +0100

    Simplify 2024 day 3 part 2

--- src/advent2024/day3/Solution.java
+++ src/advent2024/day3/Solution.java
@@ -52,51 +52,19 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public long solvePartTwo(Stream<String> lines) {
-        Pattern mulInstructions = Pattern.compile("mul\\((?<number1>\\d{1,3}),(?<number2>\\d{1,3})\\)");
+        Pattern instructions = Pattern.compile("mul\\((?<number1>\\d{1,3}),(?<number2>\\d{1,3})\\)|do\\(\\)|don't\\(\\)");
         String input = lines.collect(Collectors.joining());
-        boolean mulEnabled = true;
+
         long result = 0;
-
-        int fromIndex = 0;
-        while (fromIndex < input.length()) {
-            int doIndex = input.indexOf("do()", fromIndex);
-            doIndex = doIndex < 0 ? Integer.MAX_VALUE : doIndex;
-            int dontIndex = input.indexOf("don't()", fromIndex);
-            dontIndex = dontIndex < 0 ? Integer.MAX_VALUE : dontIndex;
-            int mulIndex;
-            long mulOperand1;
-            long mulOperand2;
-            int mulLength;
-            Matcher matcher = mulInstructions.matcher(input);
-            if (matcher.find(fromIndex)) {
-                mulIndex = matcher.start();
-                mulLength = matcher.group(0).length();
-                mulOperand1 = Long.parseLong(matcher.group("number1"));
-                mulOperand2 = Long.parseLong(matcher.group("number2"));
-            } else {
-                break;
-            }
-
-            if (doIndex < dontIndex) {
-                if (mulIndex < doIndex) { // mul first
-                    if (mulEnabled) {
-                        result += mulOperand1 * mulOperand2;
-                    }
-                    fromIndex = mulIndex + mulLength;
-                } else { // do() first
-                    mulEnabled = true;
-                    fromIndex = doIndex + "do()".length();
-                }
-            } else {
-                if (mulIndex < dontIndex) { // mul first
-                    if (mulEnabled) {
-                        result += mulOperand1 * mulOperand2;
-                    }
-                    fromIndex = mulIndex + mulLength;
-                } else { // don() first
-                    mulEnabled = false;
-                    fromIndex = dontIndex + "don't()".length();
-                }
+        boolean mulEnabled = true;
+        Matcher matcher = instructions.matcher(input);
+        while (matcher.find()) {
+            if (matcher.group(1) == null) {
+                mulEnabled = "do()".equals(matcher.group(0));
+            } else if (mulEnabled) {
+                long number1 = Long.parseLong(matcher.group("number1"));
+                long number2 = Long.parseLong(matcher.group("number2"));
+                result += number1 * number2;
             }
         }
 
