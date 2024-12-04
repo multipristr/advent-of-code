4983e42f5b1363e46ba75001a58cffa8a879e772
Tomas Hamsa <Tomas.Hamsa@ufst.dk>
2024-12-04 12:28:56 +0100

    Solve 2024 day 4

--- src/advent2024/day4/Solution.java
+++ src/advent2024/day4/Solution.java
@@ -11,28 +11,107 @@ public class Solution extends PuzzleSolver {
         new Solution().run();
     }
 
+    private static boolean containsMasInDirection(char[][] wordSearch, int rowIndex, int columnIndex, int rowDirection, int columnDirection) {
+        char[] letters = {'M', 'A', 'S'};
+        for (char letter : letters) {
+            rowIndex += rowDirection;
+            columnIndex += columnDirection;
+            if (!isLetterAtField(letter, wordSearch, rowIndex, columnIndex)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isLetterAtField(char letter, char[][] wordSearch, int rowIndex, int columnIndex) {
+        if (rowIndex < 0 || rowIndex >= wordSearch.length || columnIndex < 0) {
+            return false;
+        }
+        char[] row = wordSearch[rowIndex];
+        if (columnIndex >= row.length) {
+            return false;
+        }
+        return row[columnIndex] == letter;
+    }
+
     @Override
     public List<String> getExampleInput1() {
-        return List.of("");
+        return List.of("MMMSXXMASM\n" +
+                "MSAMXMSMSA\n" +
+                "AMXSXMAAMM\n" +
+                "MSAMASMSMX\n" +
+                "XMASAMXAMM\n" +
+                "XXAMMXXAMA\n" +
+                "SMSMSASXSS\n" +
+                "SAXAMASAAA\n" +
+                "MAMMMXMMMM\n" +
+                "MXMXAXMASX");
     }
 
     @Override
     public List<Long> getExampleOutput1() {
-        return List.of();
+        return List.of(18L);
     }
 
     @Override
     public List<Long> getExampleOutput2() {
-        return List.of();
+        return List.of(9L);
     }
 
     @Override
     public long solvePartOne(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        char[][] wordSearch = lines.map(String::toCharArray)
+                .toArray(char[][]::new);
+
+        long occurrences = 0;
+        int[] directions = {-1, 0, 1};
+        for (int rowIndex = 0; rowIndex < wordSearch.length; rowIndex++) {
+            char[] row = wordSearch[rowIndex];
+            for (int columnIndex = 0; columnIndex < row.length; columnIndex++) {
+                if (row[columnIndex] == 'X') {
+                    for (var rowDirection : directions) {
+                        for (var columnDirection : directions) {
+                            if (rowDirection == 0 && columnDirection == 0) {
+                                continue;
+                            }
+                            if (containsMasInDirection(wordSearch, rowIndex, columnIndex, rowDirection, columnDirection)) {
+                                ++occurrences;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return occurrences;
     }
 
     @Override
     public long solvePartTwo(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        char[][] wordSearch = lines.map(String::toCharArray)
+                .toArray(char[][]::new);
+
+        long occurrences = 0;
+        int[] directions = {-1, 1};
+        for (int rowIndex = 0; rowIndex < wordSearch.length; rowIndex++) {
+            char[] row = wordSearch[rowIndex];
+            for (int columnIndex = 0; columnIndex < row.length; columnIndex++) {
+                if (row[columnIndex] == 'A') {
+                    int masCount = 0;
+                    for (var rowDirection : directions) {
+                        for (var columnDirection : directions) {
+                            if (isLetterAtField('M', wordSearch, rowIndex + rowDirection, columnIndex + columnDirection)
+                                    && isLetterAtField('S', wordSearch, rowIndex - rowDirection, columnIndex - columnDirection)) {
+                                ++masCount;
+                            }
+                        }
+                    }
+                    occurrences += masCount / 2;
+                }
+            }
+        }
+
+        return occurrences;
     }
+
 }
