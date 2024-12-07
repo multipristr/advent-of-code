e2ea1fd9af628f83155fccf7d59859ba2efbdda6
MultiHunter <email@email.com>
2024-12-07 13:12:09 +0100

    Solve 2024 day 7

--- src/advent2024/day7/Solution.java
+++ src/advent2024/day7/Solution.java
@@ -2,7 +2,9 @@ package src.advent2024.day7;
 
 import src.PuzzleSolver;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.LongBinaryOperator;
 import java.util.stream.Stream;
 
 public class Solution extends PuzzleSolver {
@@ -13,26 +15,74 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public List<String> getExampleInput1() {
-        return List.of("");
+        return List.of("190: 10 19\n" +
+                "3267: 81 40 27\n" +
+                "83: 17 5\n" +
+                "156: 15 6\n" +
+                "7290: 6 8 6 15\n" +
+                "161011: 16 10 13\n" +
+                "192: 17 8 14\n" +
+                "21037: 9 7 18 13\n" +
+                "292: 11 6 16 20");
     }
 
     @Override
     public List<Long> getExampleOutput1() {
-        return List.of();
+        return List.of(3749L);
     }
 
     @Override
     public List<Long> getExampleOutput2() {
-        return List.of();
+        return List.of(11387L);
     }
 
     @Override
     public long solvePartOne(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return lines
+                .mapToLong(line -> {
+                    String[] parts = line.split(":?\\s");
+                    long testValue = Long.parseLong(parts[0]);
+                    long[] numbers = Arrays.stream(parts, 1, parts.length)
+                            .mapToLong(Long::parseLong)
+                            .toArray();
+                    return isAdditionMultiplicationTrue(testValue, numbers) ? testValue : 0;
+                })
+                .sum();
     }
 
     @Override
     public long solvePartTwo(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return lines.parallel()
+                .mapToLong(line -> {
+                    String[] parts = line.split(":?\\s");
+                    long testValue = Long.parseLong(parts[0]);
+                    long[] numbers = Arrays.stream(parts, 1, parts.length)
+                            .mapToLong(Long::parseLong)
+                            .toArray();
+                    return isAdditionMultiplicationConcatenationTrue(testValue, numbers) ? testValue : 0;
+                })
+                .sum();
+    }
+
+    private boolean isAdditionMultiplicationTrue(long testValue, long[] numbers) {
+        LongBinaryOperator[] operations = {Long::sum, (a, b) -> a * b};
+        return tryOperation(testValue, numbers, operations, 1, numbers[0]);
+    }
+
+    private boolean isAdditionMultiplicationConcatenationTrue(long testValue, long[] numbers) {
+        LongBinaryOperator[] operations = {Long::sum, (a, b) -> a * b, (a, b) -> Long.parseLong(a + "" + b)};
+        return tryOperation(testValue, numbers, operations, 1, numbers[0]);
+    }
+
+    private boolean tryOperation(long testValue, long[] numbers, LongBinaryOperator[] operations, int index, long carry) {
+        if (carry > testValue) {
+            return false;
+        }
+        if (index >= numbers.length) {
+            return testValue == carry;
+        }
+        return Arrays.stream(operations)
+                .anyMatch(operation ->
+                        tryOperation(testValue, numbers, operations, index + 1, operation.applyAsLong(carry, numbers[index])));
     }
 }
