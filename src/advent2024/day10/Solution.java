66f07e498899049da31727b56fb38bfcddaff8dc
MultiHunter <email@email.com>
2024-12-10 21:11:22 +0100

    Nicer 2024 day 10 part 2

--- src/advent2024/day10/Solution.java
+++ src/advent2024/day10/Solution.java
@@ -2,11 +2,13 @@ package src.advent2024.day10;
 
 import src.PuzzleSolver;
 
+import java.util.AbstractMap;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.IntStream;
@@ -110,31 +112,28 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public long solvePartTwo(Stream<String> lines) {
-        List<Long>[] heightLocations = IntStream.range(0, 10)
-                .mapToObj(i -> new ArrayList<>())
-                .toArray(List[]::new);
-        var lineIterator = lines.iterator();
-        List<int[]> rows = new ArrayList<>();
-        long row = 0;
-        for (; lineIterator.hasNext(); row++) {
-            String line = lineIterator.next();
-            int[] ints = new int[line.length()];
-            for (int y = 0; y < line.length(); y++) {
-                int height = Character.getNumericValue(line.charAt(y));
-                heightLocations[height].add(row << 32 | y);
-                ints[y] = height;
-            }
-            rows.add(ints);
+        int[][] topographicMap = lines.map(line -> line.chars().map(Character::getNumericValue).toArray())
+                .toArray(int[][]::new);
+        List<Map.Entry<Integer, Integer>>[] heightLocations = new List[10];
+        for (int i = 0; i < heightLocations.length; i++) {
+            heightLocations[i] = new ArrayList<>();
         }
-        int size = (int) row;
-        int[][] topographicMap = rows.toArray(int[][]::new);
-        int[][] visits = new int[size][size];
-        heightLocations[0].forEach(position -> visits[(int) (position >>> 32)][position.intValue()] = 1);
+        for (int x = 0; x < topographicMap.length; x++) {
+            int[] row = topographicMap[x];
+            for (int y = 0; y < row.length; y++) {
+                heightLocations[row[y]].add(new AbstractMap.SimpleImmutableEntry<>(x, y));
+            }
+        }
+        int[][] visits = new int[topographicMap.length][topographicMap.length];
+
+        for (var position : heightLocations[0]) {
+            visits[position.getKey()][position.getValue()] = 1;
+        }
 
         for (int height = 1; height < 9; height++) {
             for (var position : heightLocations[height]) {
-                int x = (int) (position >>> 32);
-                int y = position.intValue();
+                int x = position.getKey();
+                int y = position.getValue();
                 int newVisits = 0;
                 if (x > 0 && topographicMap[x - 1][y] == height - 1) {
                     newVisits += visits[x - 1][y];
@@ -142,10 +141,10 @@ public class Solution extends PuzzleSolver {
                 if (y > 0 && topographicMap[x][y - 1] == height - 1) {
                     newVisits += visits[x][y - 1];
                 }
-                if (y < size - 1 && topographicMap[x][y + 1] == height - 1) {
+                if (y < topographicMap.length - 1 && topographicMap[x][y + 1] == height - 1) {
                     newVisits += visits[x][y + 1];
                 }
-                if (x < size - 1 && topographicMap[x + 1][y] == height - 1) {
+                if (x < topographicMap.length - 1 && topographicMap[x + 1][y] == height - 1) {
                     newVisits += visits[x + 1][y];
                 }
                 visits[x][y] = newVisits;
@@ -154,55 +153,23 @@ public class Solution extends PuzzleSolver {
 
         long heightNineVisits = 0;
         for (var position : heightLocations[9]) {
-            int x = (int) (position >>> 32);
-            int y = position.intValue();
+            int x = position.getKey();
+            int y = position.getValue();
             if (x > 0 && topographicMap[x - 1][y] == 8) {
                 heightNineVisits += visits[x - 1][y];
             }
             if (y > 0 && topographicMap[x][y - 1] == 8) {
                 heightNineVisits += visits[x][y - 1];
             }
-            if (y < size - 1 && topographicMap[x][y + 1] == 8) {
+            if (y < topographicMap.length - 1 && topographicMap[x][y + 1] == 8) {
                 heightNineVisits += visits[x][y + 1];
             }
-            if (x < size - 1 && topographicMap[x + 1][y] == 8) {
+            if (x < topographicMap.length - 1 && topographicMap[x + 1][y] == 8) {
                 heightNineVisits += visits[x + 1][y];
             }
         }
 
         return heightNineVisits;
-    }
-
-    private long calculateTrailheadRating(int[][] topographicMap, Position trailheadStart) {
-        int[] directions = {-1, 0, 0, -1, 0, 1, 1, 0};
-        Deque<Position> open = new ArrayDeque<>();
-        long distinctHikingTrails = 0;
-
-        open.add(trailheadStart);
-        while (!open.isEmpty()) {
-            Position current = open.pollFirst();
-            for (int i = 1; i < directions.length; i += 2) {
-                int nextX = current.x + directions[i - 1];
-                if (nextX < 0 || nextX >= topographicMap.length) {
-                    continue;
-                }
-                int nextY = current.y + directions[i];
-                if (nextY < 0 || nextY >= topographicMap[nextX].length) {
-                    continue;
-                }
-                int nextHeight = topographicMap[nextX][nextY];
-                if (nextHeight == current.height + 1) {
-                    Position next = new Position(nextHeight, nextX, nextY);
-                    if (nextHeight == 9) {
-                        ++distinctHikingTrails;
-                    } else {
-                        open.addLast(next);
-                    }
-                }
-            }
-        }
-
-        return distinctHikingTrails;
     }
 
     private static final class Position {
