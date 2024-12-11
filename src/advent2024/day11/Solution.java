05fb64a8555a2c96e265cb00d9e1a05d07ca4a43
Tomas Hamsa <Tomas.Hamsa@ufst.dk>
2024-12-11 13:12:57 +0100

    Solve with list 2024 day 11

--- src/advent2024/day11/Solution.java
+++ src/advent2024/day11/Solution.java
@@ -2,7 +2,9 @@ package src.advent2024.day11;
 
 import src.PuzzleSolver;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution extends PuzzleSolver {
@@ -13,26 +15,50 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public List<String> getExampleInput1() {
-        return List.of("");
+        return List.of("125 17");
     }
 
     @Override
     public List<Long> getExampleOutput1() {
-        return List.of();
+        return List.of(55312L);
     }
 
     @Override
     public List<Long> getExampleOutput2() {
-        return List.of();
+        return List.of(0L);
     }
 
     @Override
     public long solvePartOne(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return calculateStonesAfterBlinking(lines, 25);
     }
 
     @Override
     public long solvePartTwo(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return calculateStonesAfterBlinking(lines, 75);
+    }
+
+    private int calculateStonesAfterBlinking(Stream<String> lines, int blinks) {
+        List<Long> stones = lines.map(line -> line.split("\\s+"))
+                .flatMap(Arrays::stream)
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+        for (int blink = 0; blink < blinks; blink++) {
+            for (int stoneIndex = 0; stoneIndex < stones.size(); stoneIndex++) {
+                Long stone = stones.get(stoneIndex);
+                if (stone == 0) {
+                    stones.set(stoneIndex, 1L);
+                } else if ((stone.toString().length() & 1L) == 0L) {
+                    String stoneNumber = stone.toString();
+                    int numberMiddle = stoneNumber.length() >>> 1;
+                    stones.set(stoneIndex, Long.valueOf(stoneNumber.substring(0, numberMiddle)));
+                    ++stoneIndex;
+                    stones.add(stoneIndex, Long.valueOf(stoneNumber.substring(numberMiddle)));
+                } else {
+                    stones.set(stoneIndex, stone * 2024);
+                }
+            }
+        }
+        return stones.size();
     }
 }
