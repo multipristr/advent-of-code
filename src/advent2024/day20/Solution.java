3ceb0ede4bc40e8500cd56fdd3999777fe094f60
MultiHunter <email@email.com>
2024-12-21 20:58:45 +0100

    Solve 2024 day 20 part 2

--- src/advent2024/day20/Solution.java
+++ src/advent2024/day20/Solution.java
@@ -2,10 +2,10 @@ package src.advent2024.day20;
 
 import src.PuzzleSolver;
 
+import java.util.AbstractMap;
+import java.util.ArrayDeque;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.PriorityQueue;
+import java.util.Map;
 import java.util.Queue;
 import java.util.stream.Stream;
 
@@ -46,7 +46,16 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public Comparable<?> solvePartOne(Stream<String> lines) {
-        char[][] racetrackMap = lines.map(String::toCharArray)
+        return countCheatSavingAtLeastPicoseconds(lines, 2, 100);
+    }
+
+    @Override
+    public Comparable<?> solvePartTwo(Stream<String> lines) {
+        return countCheatSavingAtLeastPicoseconds(lines, 20, 100);
+    }
+
+    private int countCheatSavingAtLeastPicoseconds(Stream<String> lines, int cheatPicoseconds, int saveAtLeastPicoseconds) {
+        var racetrackMap = lines.map(String::toCharArray)
                 .toArray(char[][]::new);
 
         int startX = 0;
@@ -54,9 +63,9 @@ public class Solution extends PuzzleSolver {
         int endX = 0;
         int endY = 0;
         for (int y = 0; y < racetrackMap.length; y++) {
-            char[] row = racetrackMap[y];
+            var row = racetrackMap[y];
             for (int x = 0; x < row.length; x++) {
-                char mark = row[x];
+                var mark = row[x];
                 if (mark == 'S') {
                     startX = x;
                     startY = y;
@@ -67,54 +76,59 @@ public class Solution extends PuzzleSolver {
             }
         }
 
-        int finalStartX = startX;
-        int finalStartY = startY;
-        int finalEndX = endX;
-        int finalEndY = endY;
-        return findPathFasterThan(racetrackMap, startX, startY, endX, endY, Integer.MAX_VALUE)
-                .map(fastestMove -> {
-                    int timeLimit = fastestMove.getPicoseconds() - 100;
-                    int savingCheats = 0;
-                    for (char[] row : racetrackMap) {
-                        for (int x = 0; x < row.length; x++) {
-                            if (row[x] == '#') {
-                                row[x] = '.';
-                                if (findPathFasterThan(racetrackMap, finalStartX, finalStartY, finalEndX, finalEndY, timeLimit).isPresent()) {
-                                    ++savingCheats;
-                                }
-                                row[x] = '#';
-                            }
+        var distancesFromEnd = calculateDistancesFrom(racetrackMap, endX, endY);
+        var distancesFromStart = calculateDistancesFrom(racetrackMap, startX, startY);
+
+        var timeLimit = distancesFromStart[endY][endX] - saveAtLeastPicoseconds;
+        int savingCheats = 0;
+        for (int y1 = 0; y1 < racetrackMap.length; y1++) {
+            var racetrackRow1 = racetrackMap[y1];
+            for (int x1 = 0; x1 < racetrackRow1.length; x1++) {
+                if (racetrackRow1[x1] == '#'
+                        || calculateManhattanDistance(x1, y1, endX, endY) >= timeLimit
+                        || calculateManhattanDistance(x1, y1, startX, startY) >= timeLimit) {
+                    continue;
+                }
+                int distanceFromStart = distancesFromStart[y1][x1];
+                if (distanceFromStart >= timeLimit) {
+                    continue;
+                }
+                for (int y2 = 0; y2 < racetrackMap.length; y2++) {
+                    var racetrackRow2 = racetrackMap[y2];
+                    for (int x2 = 0; x2 < racetrackRow2.length; x2++) {
+                        if (racetrackRow2[x2] == '#') {
+                            continue;
+                        }
+                        int cheatDistance = calculateManhattanDistance(x1, y1, x2, y2);
+                        if (cheatDistance <= cheatPicoseconds && distanceFromStart + cheatDistance + distancesFromEnd[y2][x2] <= timeLimit) {
+                            ++savingCheats;
                         }
                     }
-                    return savingCheats;
-                })
-                .orElseThrow();
+                }
+            }
+        }
+        return savingCheats;
     }
 
-    @Override
-    public Comparable<?> solvePartTwo(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    private int calculateManhattanDistance(int x1, int y1, int x2, int y2) {
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
 
-    private Optional<Move> findPathFasterThan(char[][] racetrackMap, int startX, int startY, int endX, int endY, int timeLimit) {
+    private int[][] calculateDistancesFrom(char[][] racetrackMap, int x, int y) {
         byte[] directions = {-1, 0, 0, -1, 0, 1, 1, 0};
-        boolean[][] closed = new boolean[racetrackMap.length][racetrackMap[0].length];
-        closed[startY][startX] = true;
-        var startMove = new Move(endX, endY, startX, startY, null, 0);
-        Queue<Move> open = new PriorityQueue<>();
+        var distances = new int[racetrackMap.length][racetrackMap[0].length];
+        var startMove = new AbstractMap.SimpleImmutableEntry<>(x, y);
+        Queue<Map.Entry<Integer, Integer>> open = new ArrayDeque<>();
         open.add(startMove);
         while (!open.isEmpty()) {
             var current = open.poll();
-            if (current.getX() == endX && current.getY() == endY) {
-                return Optional.of(current);
-            }
 
             for (byte i = 1; i < directions.length; i += 2) {
-                var nextY = current.getY() + directions[i - 1];
+                var nextY = current.getValue() + directions[i - 1];
                 if (nextY < 0 || nextY >= racetrackMap.length) {
                     continue;
                 }
-                var nextX = current.getX() + directions[i];
+                var nextX = current.getKey() + directions[i];
                 if (nextX < 0) {
                     continue;
                 }
@@ -122,74 +136,16 @@ public class Solution extends PuzzleSolver {
                 if (nextX >= racetrackRow.length || racetrackRow[nextX] == '#') {
                     continue;
                 }
-                boolean[] closedRow = closed[nextY];
-                if (!closedRow[nextX]) {
-                    Move nextMove = new Move(endX, endY, nextX, nextY, current, current.getPicoseconds() + 1);
-                    if (nextMove.getRemainingDistance() + nextMove.getPicoseconds() <= timeLimit) {
-                        closedRow[nextX] = true;
-                        open.add(nextMove);
-                    }
+                var distanceRow = distances[nextY];
+                if (distanceRow[nextX] == 0) {
+                    distanceRow[nextX] = distances[current.getValue()][current.getKey()] + 1;
+                    open.add(new AbstractMap.SimpleImmutableEntry<>(nextX, nextY));
                 }
             }
         }
+        distances[y][x] = 0;
 
-        return Optional.empty();
-    }
-
-    private static class Move implements Comparable<Move> {
-
-        private final int x;
-        private final int y;
-        private final Move previous;
-        private final int picoseconds;
-        private final int remainingDistance;
-
-        private Move(int endX, int endY, int x, int y, Move previous, int picoseconds) {
-            this.y = y;
-            this.x = x;
-            this.picoseconds = picoseconds;
-            this.previous = previous;
-            remainingDistance = Math.abs(x - endX) + Math.abs(endY - y);
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public int getPicoseconds() {
-            return picoseconds;
-        }
-
-        public Optional<Move> getPrevious() {
-            return Optional.ofNullable(previous);
-        }
-
-        public int getRemainingDistance() {
-            return remainingDistance;
-        }
-
-        @Override
-        public int compareTo(final Move o) {
-            return (remainingDistance + picoseconds) - (o.remainingDistance + o.picoseconds);
-        }
-
-        @Override
-        public boolean equals(final Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Move move = (Move) o;
-            return x == move.x && y == move.y && picoseconds == move.picoseconds;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y, picoseconds);
-        }
-
+        return distances;
     }
 
 }
