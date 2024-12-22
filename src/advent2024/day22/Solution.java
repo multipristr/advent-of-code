939e68f428db31a2df6fd87bd12b715c6dc751b4
MultiHunter <email@email.com>
2024-12-22 11:24:17 +0100

    Solve 2024 day 22 part 1

--- src/advent2024/day22/Solution.java
+++ src/advent2024/day22/Solution.java
@@ -13,12 +13,15 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public List<String> getExampleInput1() {
-        return List.of("");
+        return List.of("1\n" +
+                "10\n" +
+                "100\n" +
+                "2024");
     }
 
     @Override
     public List<Comparable<?>> getExampleOutput1() {
-        return List.of();
+        return List.of(37327623);
     }
 
     @Override
@@ -28,11 +31,28 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public Comparable<?> solvePartOne(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return lines.parallel()
+                .mapToLong(Long::parseLong)
+                .map(this::generateSecretNumber)
+                .sum();
     }
 
     @Override
     public Comparable<?> solvePartTwo(Stream<String> lines) {
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private long generateSecretNumber(long initialSecretNumber) {
+        for (short i = 0; i < 2_000; i++) {
+            initialSecretNumber ^= initialSecretNumber << 6;
+            initialSecretNumber %= 16777216;
+
+            initialSecretNumber ^= initialSecretNumber >>> 5;
+            initialSecretNumber %= 16777216;
+
+            initialSecretNumber ^= initialSecretNumber << 11;
+            initialSecretNumber %= 16777216;
+        }
+        return initialSecretNumber;
     }
 }
