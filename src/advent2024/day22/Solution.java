10bbfdbb10516f7a93ae635ae043ef22676d2eef
MultiHunter <email@email.com>
2024-12-22 12:18:01 +0100

    Solve 2024 day 22 part 2

--- src/advent2024/day22/Solution.java
+++ src/advent2024/day22/Solution.java
@@ -2,7 +2,13 @@ package src.advent2024.day22;
 
 import src.PuzzleSolver;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringJoiner;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
 public class Solution extends PuzzleSolver {
@@ -25,34 +31,82 @@ public class Solution extends PuzzleSolver {
     }
 
     @Override
+    public List<String> getExampleInput2() {
+        return List.of("1\n" +
+                "2\n" +
+                "3\n" +
+                "2024");
+    }
+
+    @Override
     public List<Comparable<?>> getExampleOutput2() {
-        return List.of();
+        return List.of(23);
     }
 
     @Override
     public Comparable<?> solvePartOne(Stream<String> lines) {
         return lines.parallel()
                 .mapToLong(Long::parseLong)
-                .map(this::generateSecretNumber)
+                .map(initialSecretNumber -> {
+                    for (short i = 0; i < 2000; i++) {
+                        initialSecretNumber = generateNextSecretNumber(initialSecretNumber);
+                    }
+                    return initialSecretNumber;
+                })
                 .sum();
     }
 
     @Override
     public Comparable<?> solvePartTwo(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Map<String, Long> sequences = new ConcurrentHashMap<>();
+        lines.parallel()
+                .mapToLong(Long::parseLong)
+                .forEach(initialSecretNumber -> calculateSequenceBananas(sequences, initialSecretNumber));
+
+        return sequences.values().stream().max(Comparator.naturalOrder()).orElseThrow();
     }
 
-    private long generateSecretNumber(long initialSecretNumber) {
-        for (short i = 0; i < 2_000; i++) {
-            initialSecretNumber ^= initialSecretNumber << 6;
-            initialSecretNumber %= 16777216;
+    private void calculateSequenceBananas(Map<String, Long> sequences, long initialSecretNumber) {
+        var circularQueue = new String[4];
+        int head = 0;
+        int tail = 0;
+        Set<String> seenSequences = new HashSet<>();
+        var previousLastDigit = initialSecretNumber % 10;
 
-            initialSecretNumber ^= initialSecretNumber >>> 5;
-            initialSecretNumber %= 16777216;
+        for (short i = 0; i < 2000; i++) {
+            var secretNumber = generateNextSecretNumber(initialSecretNumber);
+            var lastDigit = secretNumber % 10;
+            var priceChange = lastDigit - previousLastDigit;
 
-            initialSecretNumber ^= initialSecretNumber << 11;
-            initialSecretNumber %= 16777216;
+            circularQueue[tail] = "" + priceChange;
+            tail = (tail + 1) % circularQueue.length;
+            if (head == tail) {
+                StringJoiner stringJoiner = new StringJoiner(",");
+                for (int j = 0; j < circularQueue.length; j++) {
+                    stringJoiner.add(circularQueue[(head + j) % circularQueue.length]);
+                }
+                var sequence = stringJoiner.toString();
+                if (seenSequences.add(sequence)) {
+                    sequences.merge(sequence, lastDigit, Long::sum);
+                }
+                head = (head + 1) % circularQueue.length;
+            }
+
+            initialSecretNumber = secretNumber;
+            previousLastDigit = lastDigit;
         }
-        return initialSecretNumber;
+    }
+
+    private long generateNextSecretNumber(long secretNumber) {
+        secretNumber ^= secretNumber << 6;
+        secretNumber %= 16777216;
+
+        secretNumber ^= secretNumber >> 5;
+        secretNumber %= 16777216;
+
+        secretNumber ^= secretNumber << 11;
+        secretNumber %= 16777216;
+
+        return secretNumber;
     }
 }
