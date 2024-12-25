c710f456296a740d83f016c941334db3c15c5e86
Tomas Hamsa <Tomas.Hamsa@ufst.dk>
2024-12-25 11:26:25 +0100

    Solve 2024 day 25

--- src/advent2024/day25/Solution.java
+++ src/advent2024/day25/Solution.java
@@ -2,7 +2,10 @@ package src.advent2024.day25;
 
 import src.PuzzleSolver;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution extends PuzzleSolver {
@@ -13,12 +16,50 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public List<String> getExampleInput1() {
-        return List.of("");
+        return List.of("#####\n" +
+                ".####\n" +
+                ".####\n" +
+                ".####\n" +
+                ".#.#.\n" +
+                ".#...\n" +
+                ".....\n" +
+                "\n" +
+                "#####\n" +
+                "##.##\n" +
+                ".#.##\n" +
+                "...##\n" +
+                "...#.\n" +
+                "...#.\n" +
+                ".....\n" +
+                "\n" +
+                ".....\n" +
+                "#....\n" +
+                "#....\n" +
+                "#...#\n" +
+                "#.#.#\n" +
+                "#.###\n" +
+                "#####\n" +
+                "\n" +
+                ".....\n" +
+                ".....\n" +
+                "#.#..\n" +
+                "###..\n" +
+                "###.#\n" +
+                "###.#\n" +
+                "#####\n" +
+                "\n" +
+                ".....\n" +
+                ".....\n" +
+                ".....\n" +
+                "#....\n" +
+                "#.#..\n" +
+                "#.#.#\n" +
+                "#####");
     }
 
     @Override
     public List<Comparable<?>> getExampleOutput1() {
-        return List.of();
+        return List.of(3);
     }
 
     @Override
@@ -28,11 +69,44 @@ public class Solution extends PuzzleSolver {
 
     @Override
     public Comparable<?> solvePartOne(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        var schematics = lines.collect(Collectors.joining(System.lineSeparator())).split("\\R{2}");
+
+        List<int[]> lockHeights = new ArrayList<>();
+        List<int[]> keyHeights = new ArrayList<>();
+        int schematicHeight = 0;
+        for (var schematic : schematics) {
+            var schematicRows = schematic.split("\\R");
+            schematicHeight = schematicRows.length;
+            var topRow = schematicRows[0];
+            boolean lock = topRow.codePoints().allMatch(c -> (char) c == '#');
+            var heights = new int[topRow.length()];
+            for (var schematicRow : schematicRows) {
+                for (int column = 0; column < schematicRow.length(); column++) {
+                    if (schematicRow.charAt(column) == '#') {
+                        ++heights[column];
+                    }
+                }
+            }
+            if (lock) {
+                lockHeights.add(heights);
+            } else {
+                keyHeights.add(heights);
+            }
+        }
+
+        int finalSchematicHeight = schematicHeight;
+        return lockHeights.parallelStream()
+                .flatMap(lockHeight -> keyHeights.stream()
+                        .filter(keyHeight -> IntStream.range(0, keyHeight.length)
+                                .noneMatch(column -> lockHeight[column] + keyHeight[column] > finalSchematicHeight)
+                        )
+                )
+                .count();
     }
 
     @Override
     public Comparable<?> solvePartTwo(Stream<String> lines) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
 }
