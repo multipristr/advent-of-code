fb43c881f8d4a4b98db5197a4867ba4f4507c6b8
Tomas Hamsa <Tomas.Hamsa@ufst.dk>
2025-02-03 13:20:32 +0100

    Prepare 2025

---
+++ src/advent2025/day10/Solution.java
@@ -0,0 +1,38 @@
+package src.advent2025.day10;
+
+import src.PuzzleSolver;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Solution extends PuzzleSolver {
+
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    @Override
+    public List<String> getExampleInput1() {
+        return List.of("");
+    }
+
+    @Override
+    public List<Comparable<?>> getExampleOutput1() {
+        return List.of();
+    }
+
+    @Override
+    public List<Comparable<?>> getExampleOutput2() {
+        return List.of();
+    }
+
+    @Override
+    public Comparable<?> solvePartOne(Stream<String> lines) {
+       throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Comparable<?> solvePartTwo(Stream<String> lines) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
