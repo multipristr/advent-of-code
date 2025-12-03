37b1c061f66258913d6a3e60f3e59653c0e34a82
Name Surname <email@email.net>
2025-12-03 21:44:58 +0100

    Solve 2025 day 2 part 1

--- src/advent2025/day2/Solution.java
+++ src/advent2025/day2/Solution.java
@@ -2,7 +2,9 @@ package src.advent2025.day2;
 
 import src.PuzzleSolver;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Solution extends PuzzleSolver<Long, Long> {
@@ -23,12 +25,34 @@ public class Solution extends PuzzleSolver<Long, Long> {
 
     @Override
     public List<Long> getExampleOutput2() {
-        return List.of();
+        return List.of(4174379265L);
     }
 
     @Override
     public Long solvePartOne(Stream<String> lines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return lines.parallel()
+                .flatMap(line -> Arrays.stream(line.split(",")))
+                .flatMapToLong(range -> {
+                    var dashIndex = range.indexOf("-");
+                    var firstId = range.substring(0, dashIndex);
+                    var lastId = range.substring(dashIndex + 1);
+                    return findInvalidIdsInRange(Long.parseLong(firstId), Long.parseLong(lastId));
+                })
+                .sum();
+    }
+
+    private LongStream findInvalidIdsInRange(long firstId, long lastId) {
+        var streamBuilder = LongStream.builder();
+        for (long id = firstId; id <= lastId; id++) {
+            var idString = Long.toString(id);
+            var middleIndex = idString.length() >> 1;
+            var firstHalf = idString.substring(0, middleIndex);
+            var secondHalf = idString.substring(middleIndex);
+            if (firstHalf.equals(secondHalf)) {
+                streamBuilder.add(id);
+            }
+        }
+        return streamBuilder.build();
     }
 
     @Override
