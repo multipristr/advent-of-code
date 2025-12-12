8a8a5a5bf3a9a49c8fb26055dbfe2a7eb2cb1976
Name Surname <email@email.net>
2025-12-12 08:41:16 +0100

    Prepare 2025 day 12

--- src/advent2025/day12/Solution.java
+++ src/advent2025/day12/Solution.java
@@ -13,12 +13,44 @@ public class Solution extends PuzzleSolver<Long, Long> {
 
     @Override
     public List<String> getExampleInput1() {
-        return List.of("");
+        return List.of("0:\n" +
+                "###\n" +
+                "##.\n" +
+                "##.\n" +
+                "\n" +
+                "1:\n" +
+                "###\n" +
+                "##.\n" +
+                ".##\n" +
+                "\n" +
+                "2:\n" +
+                ".##\n" +
+                "###\n" +
+                "##.\n" +
+                "\n" +
+                "3:\n" +
+                "##.\n" +
+                "###\n" +
+                "##.\n" +
+                "\n" +
+                "4:\n" +
+                "###\n" +
+                "#..\n" +
+                "###\n" +
+                "\n" +
+                "5:\n" +
+                "###\n" +
+                ".#.\n" +
+                "###\n" +
+                "\n" +
+                "4x4: 0 0 0 0 2 0\n" +
+                "12x5: 1 0 1 0 2 2\n" +
+                "12x5: 1 0 1 0 3 2");
     }
 
     @Override
     public List<Long> getExampleOutput1() {
-        return List.of();
+        return List.of(2L);
     }
 
     @Override
