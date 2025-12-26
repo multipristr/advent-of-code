7fb39c8e2c1eea4aa92afb9e54c255246bfd03bc
Name Surname <email@email.net>
2025-12-27 00:28:09 +0100

    Prepare 2025 day 12 part 1

--- src/advent2025/day12/Solution.java
+++ src/advent2025/day12/Solution.java
@@ -2,12 +2,11 @@ package src.advent2025.day12;
 
 import src.PuzzleSolver;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.LongFunction;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution extends PuzzleSolver<Long, Long> {
@@ -68,7 +67,7 @@ public class Solution extends PuzzleSolver<Long, Long> {
     public Long solvePartOne(Stream<String> lines) {
         var inputParts = lines.collect(Collectors.joining("\n")).split("\\R{2,}");
 
-        var presentShapes = new Region[inputParts.length - 1];
+        var presentShapes = new boolean[inputParts.length - 1][][];
         for (int shapeIndex = 0; shapeIndex < inputParts.length - 1; shapeIndex++) {
             var shapeLines = inputParts[shapeIndex].split("\\R");
             var presentShape = new boolean[shapeLines.length - 1][];
@@ -82,11 +81,10 @@ public class Solution extends PuzzleSolver<Long, Long> {
                 }
                 presentShape[shapeLine - 1] = presentShapeLine;
             }
-            presentShapes[shapeIndex] = new Region(presentShape);
+            presentShapes[shapeIndex] = presentShape;
         }
 
         var shapeVariants = generateShapeVariants(presentShapes);
-        Map<SearchState, Boolean> memory = new ConcurrentHashMap<>();
 
         return inputParts[inputParts.length - 1].lines().parallel()
                 .map(REGION_PATTERN::matcher)
@@ -94,14 +92,14 @@ public class Solution extends PuzzleSolver<Long, Long> {
                     regionMatcher.find();
                     var width = Integer.parseInt(regionMatcher.group("width"));
                     var length = Integer.parseInt(regionMatcher.group("length"));
-                    var region = new Region(new boolean[length][width]);
+                    var region = new boolean[length][width];
 
                     var shapeQuantitiesInput = regionMatcher.group("shapeQuantities").split("\\s");
                     var shapeQuantities = Arrays.stream(shapeQuantitiesInput)
                             .mapToLong(Long::parseLong)
                             .toArray();
 
-                    boolean b = canAllPresentsFitInRegion(shapeVariants, region, shapeQuantities, memory);
+                    boolean b = canAllPresentsFitInRegion(shapeVariants, region, shapeQuantities);
                     if (b) {
                         System.out.println("✅ " + width + "x" + length + ": " + String.join(" ", shapeQuantitiesInput));
                     } else {
@@ -112,195 +110,134 @@ public class Solution extends PuzzleSolver<Long, Long> {
                 .count();
     }
 
-    private boolean canAllPresentsFitInRegion(Collection<Region>[] shapeVariants, Region region, long[] presentQuantities, Map<SearchState, Boolean> memory) {
+    private boolean canAllPresentsFitInRegion(List<boolean[][]>[] shapeVariants, boolean[][] region, long[] presentQuantities) {
         for (int present = 0; present < presentQuantities.length; present++) {
             if (presentQuantities[present] > 0) {
-                var finalPresent = present;
-                return shapeVariants[present].parallelStream()
-                        .anyMatch(shape -> {
-                            var columns = region.shape.length - shape.shape.length;
-                            var rows = region.shape[0].length - shape.shape[0].length;
-                            for (int fromColumn = 0; fromColumn <= columns; fromColumn++) {
-                                for (int fromRow = 0; fromRow <= rows; fromRow++) {
-                                    if (region.canFit(shape, fromColumn, fromRow)) {
-                                        var nextRegion = region.fit(shape, fromColumn, fromRow);
-                                        var nextPresentQuantities = Arrays.copyOf(presentQuantities, presentQuantities.length);
-                                        --nextPresentQuantities[finalPresent];
-
-                                        var memoryKey = new SearchState(nextPresentQuantities, nextRegion);
-                                        var saved = memory.get(memoryKey);
-                                        if (saved == null) {
-                                            saved = canAllPresentsFitInRegion(shapeVariants, nextRegion, nextPresentQuantities, memory);
-                                            memory.put(memoryKey, saved);
-                                        }
-                                        if (saved) {
-                                            return true;
-                                        }
-                                    }
-                                }
-                            }
-                            return false;
-                        });
+                for (var shape : shapeVariants[present]) {
+                    if (canPresentFitInRegion(shapeVariants, region, presentQuantities, shape, present)) {
+                        return true;
+                    }
+                }
+                return false;
             }
         }
         return true;
     }
 
-    private Collection<Region>[] generateShapeVariants(Region[] presents) {
-        Collection<Region>[] shapeVariants = new Set[presents.length];
+    private boolean canPresentFitInRegion(List<boolean[][]>[] shapeVariants, boolean[][] region, long[] presentQuantities, boolean[][] shape, int present) {
+        var columns = region.length - shape.length;
+        var rows = region[0].length - shape[0].length;
+
+        for (int fromColumn = 0; fromColumn <= columns; fromColumn++) {
+            for (int fromRow = 0; fromRow <= rows; fromRow++) {
+                if (fitInRegion(region, shape, fromColumn, fromRow)) {
+                    --presentQuantities[present];
+                    if (canAllPresentsFitInRegion(shapeVariants, region, presentQuantities)) {
+                        return true;
+                    }
+                    ++presentQuantities[present];
+                    removeFromRegion(region, shape, fromColumn, fromRow);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean fitInRegion(boolean[][] region, boolean[][] shape, int fromColumn, int fromRow) {
+        for (int c = 0; c < shape.length; c++) {
+            var shapeColumn = shape[c];
+            var regionColumn = region[c + fromColumn];
+            for (int r = 0; r < shapeColumn.length; r++) {
+                if (shapeColumn[r] && regionColumn[r + fromRow]) {
+                    return false;
+                }
+            }
+        }
+        for (int c = 0; c < shape.length; c++) {
+            var shapeColumn = shape[c];
+            var regionColumn = region[c + fromColumn];
+            for (int r = 0; r < shapeColumn.length; r++) {
+                regionColumn[r + fromRow] |= shapeColumn[r];
+            }
+        }
+        return true;
+    }
+
+    private void removeFromRegion(boolean[][] region, boolean[][] shape, int fromColumn, int fromRow) {
+        for (int c = 0; c < shape.length; c++) {
+            var shapeColumn = shape[c];
+            var regionColumn = region[c + fromColumn];
+            for (int r = 0; r < shapeColumn.length; r++) {
+                regionColumn[r + fromRow] ^= shapeColumn[r];
+            }
+        }
+    }
+
+    private List<boolean[][]>[] generateShapeVariants(boolean[][][] presents) {
+        List<boolean[][]>[] shapeVariants = new List[presents.length];
         for (int present = 0; present < presents.length; present++) {
             var shape = presents[present];
-            Collection<Region> variants = new HashSet<>(12);
+            List<boolean[][]> variants = new ArrayList<>(12);
             addAllRotated(variants, shape);
-            addAllRotated(variants, shape.flipHorizontally());
-            addAllRotated(variants, shape.flipVertically());
+            addAllRotated(variants, flipHorizontally(shape));
+            addAllRotated(variants, flipVertically(shape));
             shapeVariants[present] = variants;
         }
         return shapeVariants;
     }
 
-    private void addAllRotated(Collection<Region> variants, Region shape) {
+    private void addAllRotated(List<boolean[][]> variants, boolean[][] shape) {
         var rotated = shape;
-        variants.add(rotated);
-        for (int rotation = 0; rotation < 3; rotation++) {
-            rotated = rotated.rotate();
-            variants.add(rotated);
+        var finalRotated = rotated;
+        if (variants.stream().noneMatch(variant -> Arrays.deepEquals(finalRotated, variant))) {
+            variants.add(finalRotated);
         }
+        for (int rotation = 0; rotation < 3; rotation++) {
+            rotated = rotate(rotated);
+            var finalRotated2 = rotated;
+            if (variants.stream().noneMatch(variant -> Arrays.deepEquals(finalRotated2, variant))) {
+                variants.add(finalRotated2);
+            }
+        }
+    }
+
+    private boolean[][] flipHorizontally(boolean[][] shape) {
+        var rows = shape.length;
+        var columns = shape[0].length;
+        var flipped = new boolean[rows][columns];
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                flipped[r][c] = shape[r][columns - 1 - c];
+            }
+        }
+        return flipped;
+    }
+
+    private boolean[][] flipVertically(boolean[][] shape) {
+        var rows = shape.length;
+        var columns = shape[0].length;
+        var flipped = new boolean[rows][columns];
+        for (int r = 0; r < rows; r++) {
+            System.arraycopy(shape[rows - 1 - r], 0, flipped[r], 0, columns);
+        }
+        return flipped;
+    }
+
+    private boolean[][] rotate(boolean[][] shape) {
+        var rows = shape.length;
+        var columns = shape[0].length;
+        var rotated = new boolean[columns][rows];
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                rotated[c][rows - 1 - r] = shape[r][c];
+            }
+        }
+        return rotated;
     }
 
     @Override
     public Long solvePartTwo(Stream<String> lines) {
         throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    private static final class Region {
-        private final boolean[][] shape;
-
-        Region(boolean[][] shape) {
-            this.shape = shape;
-        }
-
-        private boolean canFit(Region that, int fromColumn, int fromRow) {
-            for (int c = 0; c < that.shape.length; c++) {
-                var shapeColumn = that.shape[c];
-                var regionColumn = shape[c + fromColumn];
-                for (int r = 0; r < shapeColumn.length; r++) {
-                    if (shapeColumn[r] && regionColumn[r + fromRow]) {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-
-        private Region fit(Region that, int fromColumn, int fromRow) {
-            var rows = shape.length;
-            var fitted = new boolean[rows][];
-            for (int r = 0; r < rows; r++) {
-                var columns = shape[r].length;
-                fitted[r] = new boolean[columns];
-                System.arraycopy(shape[r], 0, fitted[r], 0, columns);
-            }
-            for (int c = 0; c < that.shape.length; c++) {
-                var shapeColumn = that.shape[c];
-                var fittedColumn = fitted[c + fromColumn];
-                for (int r = 0; r < shapeColumn.length; r++) {
-                    fittedColumn[r + fromRow] |= shapeColumn[r];
-                }
-            }
-            return new Region(fitted);
-        }
-
-        private Region flipHorizontally() {
-            var rows = shape.length;
-            var flipped = new boolean[rows][];
-            for (int r = 0; r < rows; r++) {
-                var columns = shape[r].length;
-                var flipRow = new boolean[columns];
-                var shapeRow = shape[r];
-                for (int c = 0; c < columns; c++) {
-                    flipRow[c] = shapeRow[columns - 1 - c];
-                }
-                flipped[r] = flipRow;
-            }
-            return new Region(flipped);
-        }
-
-        private Region flipVertically() {
-            var rows = shape.length;
-            var flipped = new boolean[rows][];
-            for (int r = 0; r < rows; r++) {
-                var columns = shape[r].length;
-                flipped[r] = new boolean[columns];
-                System.arraycopy(shape[rows - 1 - r], 0, flipped[r], 0, columns);
-            }
-            return new Region(flipped);
-        }
-
-        private Region rotate() {
-            var rows = shape.length;
-            var columns = shape[0].length;
-            var rotated = new boolean[columns][rows];
-            for (int r = 0; r < rows; r++) {
-                var shapeRow = shape[r];
-                for (int c = 0; c < columns; c++) {
-                    rotated[c][rows - 1 - r] = shapeRow[c];
-                }
-            }
-            return new Region(rotated);
-        }
-
-        @Override
-        public boolean equals(Object that) {
-            if (that == null || getClass() != that.getClass()) return false;
-            Region region = (Region) that;
-            return Arrays.deepEquals(shape, region.shape);
-        }
-
-        @Override
-        public int hashCode() {
-            return Arrays.deepHashCode(shape);
-        }
-
-        @Override
-        public String toString() {
-            var text = new StringJoiner("\n");
-            for (var row : shape) {
-                var rowText = new StringBuilder(row.length);
-                for (var place : row) {
-                    rowText.append(place ? "#" : ".");
-                }
-                text.add(rowText.toString());
-            }
-            return text.toString();
-        }
-    }
-
-    private static final class SearchState {
-        private final long[] presentQuantities;
-        private final Region region;
-
-        private SearchState(long[] presentQuantities, Region region) {
-            this.presentQuantities = presentQuantities;
-            this.region = region;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (o == null || getClass() != o.getClass()) return false;
-            SearchState that = (SearchState) o;
-            return Objects.deepEquals(presentQuantities, that.presentQuantities) && Objects.equals(region, that.region);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(Arrays.hashCode(presentQuantities), region);
-        }
-
-        @Override
-        public String toString() {
-            return region.shape[0].length + "x" + region.shape.length + ": " + Arrays.stream(presentQuantities).mapToObj(Long::toString).collect(Collectors.joining(" "))
-                    + System.lineSeparator() + region;
-        }
     }
 }
